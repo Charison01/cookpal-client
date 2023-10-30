f2a5ebe4a98ee44a6302f9ca5d1d49fc9098e524
kelvin kimaru <kelvinkimaru@gmail.com>
2023-10-30 18:35:08 +0300

    add context provider

--- src/Context/Provider.js
+++ src/Context/Provider.js
@@ -1,1 +1,19 @@
 import { createContext, useContext } from "react";
+const AppContext = createContext("");
+
+export const useAppContext = () => useContext(AppContext);
+
+export default function ContextProvider({ children }) {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    //fetch /me
+  }, []);
+
+  const context = {
+    user,
+    setUser,
+  };
+
+  return <AppContext.Provider value={context}>{children}</AppContext.Provider>;
+}
