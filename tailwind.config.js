560bba262d2aaf21eb4e5da12fde64017538976d
kelvin kimaru <kelvinkimaru@gmail.com>
2023-10-30 18:24:57 +0300

    update theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -4,6 +4,9 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
     "node_modules/flowbite-react/**/*.{js,jsx,ts,tsx}",
   ],
+  daisyui: {
+    themes: ["light", "dark", "winter", "cupcake"],
+  },
   theme: {
     extend: {},
   },
