3d98fee871fe8e11d327a6009a0383b3d4cec831
kelvin kimaru <kelvinkimaru@gmail.com>
2023-10-31 02:24:50 +0300

    rearraged navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -213,6 +213,31 @@ export default function Sidebar() {
               <span className="tooltip__content">Community</span>
             </a>
           </li>
+
+          <li>
+            <a
+              href="/help"
+              title="Help"
+              className={`tooltip ${activeLink === "/help" ? "active" : ""}`}>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                width="24"
+                height="24"
+                viewBox="0 0 24 24"
+                strokeWidth="2"
+                stroke="currentColor"
+                fill="none"
+                strokeLinecap="round"
+                strokeLinejoin="round">
+                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
+                <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path>
+                <path d="M12 16v.01"></path>
+                <path d="M12 13a2 2 0 0 0 .914 -3.782a1.98 1.98 0 0 0 -2.414 .483"></path>
+              </svg>
+              <span className="link hide">Help</span>
+              <span className="tooltip__content">Help</span>
+            </a>
+          </li>
           <li>
             <a
               href="/settings"
@@ -236,30 +261,6 @@ export default function Sidebar() {
               </svg>
               <span className="link hide">Settings</span>
               <span className="tooltip__content">Settings</span>
-            </a>
-          </li>
-          <li>
-            <a
-              href="/help"
-              title="Help"
-              className={`tooltip ${activeLink === "/help" ? "active" : ""}`}>
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                width="24"
-                height="24"
-                viewBox="0 0 24 24"
-                strokeWidth="2"
-                stroke="currentColor"
-                fill="none"
-                strokeLinecap="round"
-                strokeLinejoin="round">
-                <path stroke="none" d="M0 0h24v24H0z" fill="none"></path>
-                <path d="M3 12a9 9 0 1 0 18 0a9 9 0 0 0 -18 0"></path>
-                <path d="M12 16v.01"></path>
-                <path d="M12 13a2 2 0 0 0 .914 -3.782a1.98 1.98 0 0 0 -2.414 .483"></path>
-              </svg>
-              <span className="link hide">Help</span>
-              <span className="tooltip__content">Help</span>
             </a>
           </li>
         </ul>
