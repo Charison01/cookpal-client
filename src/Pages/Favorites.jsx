4ec0e35536fc468d77ec5018e37043b7c153db8e
kelvin kimaru <kelvinkimaru@gmail.com>
2023-10-31 20:58:33 +0300

    updated community page

--- src/Pages/Favorites.jsx
+++ src/Pages/Favorites.jsx
@@ -1,7 +1,20 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useAppContext } from "../Context/Provider";
+import { showLoginPopup } from "../lib";
+import { useNavigate } from "react-router-dom";
 
 export default function Favorites() {
+  const { user } = useAppContext();
+  const navigate = useNavigate();
+  if (!user) {
+    showLoginPopup();
+    navigate("/");
+  }
   return (
-    <div>Favorites</div>
-  )
+    <section className="px-2 flex-1 lg:max-w-[75%] lg:mx-auto">
+      <h1 className="text-2xl lg:text-3xl font-bold text-gray-600">
+        Here are your favorite recipes
+      </h1>
+    </section>
+  );
 }
