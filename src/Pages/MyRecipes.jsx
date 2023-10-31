de6a8f8fc01f38fe957be8a8c75c2732ec867982
kelvin kimaru <kelvinkimaru@gmail.com>
2023-10-31 21:25:17 +0300

    add babel plugin react

--- src/Pages/MyRecipes.jsx
+++ src/Pages/MyRecipes.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-
+import { Recipecard } from "../Components";
 export default function MyRecipes() {
   const [myRecipes, setMyRecipes] = useState([]);
   const userId = sessionStorage.getItem("user_id");
@@ -20,7 +20,7 @@ export default function MyRecipes() {
         console.error(error);
       }
     }
-  }, []);
+  }, [userId]);
 
   return (
     <section className="px-2 flex-1 lg:max-w-[75%] lg:mx-auto bg-[url('https://images.unsplash.com/photo-1490818387583-1baba5e638af?auto=format&fit=crop&q=80&w=1932&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-center bg-opacity-[65%]">
@@ -28,6 +28,20 @@ export default function MyRecipes() {
         {" "}
         My Recipes
       </h1>
+      {/* section for creating new recipes */}
+      {/* section for rendering recipe cards */}
+      <section className="py-2 px-2 recipecard-grid-container ">
+        {/* set the grid to auto-rows */}
+        {myRecipes && myRecipes.length > 0 ? (
+          myRecipes.map((recipe) => (
+            <Recipecard key={recipe.id} recipe={recipe} />
+          ))
+        ) : (
+          <h1 className="font-bold alert-info">
+            You have no Recipes. Get Creating!
+          </h1>
+        )}
+      </section>
     </section>
   );
 }
