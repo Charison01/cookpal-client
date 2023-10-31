7f04e746a22be29523925c51bd96ac17c64a5063
kelvin kimaru <kelvinkimaru@gmail.com>
2023-10-31 04:22:09 +0300

    added my recipes route

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -137,6 +137,32 @@ export default function Sidebar() {
               <span className="tooltip__content">Explore</span>
             </a>
           </li>
+          {user ? (
+            <li>
+              <a
+                href="/my-recipes"
+                title="my-recipes"
+                className={`tooltip ${
+                  activeLink === "/my-recipes" ? "active" : ""
+                }`}>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  width="24"
+                  height="24"
+                  viewBox="0 0 24 24"
+                  fill="none"
+                  stroke="currentColor"
+                  strokeWidth="2"
+                  strokeLinecap="round"
+                  strokeLinejoin="round">
+                  <path d="M3.85 8.62a4 4 0 0 1 4.78-4.77 4 4 0 0 1 6.74 0 4 4 0 0 1 4.78 4.78 4 4 0 0 1 0 6.74 4 4 0 0 1-4.77 4.78 4 4 0 0 1-6.75 0 4 4 0 0 1-4.78-4.77 4 4 0 0 1 0-6.76Z" />
+                  <path d="m9 12 2 2 4-4" />
+                </svg>
+                <span className="link hide">My Recipes</span>
+                <span className="tooltip__content">My Recipes</span>
+              </a>
+            </li>
+          ) : null}
           <li>
             <a
               href="/favorites"
