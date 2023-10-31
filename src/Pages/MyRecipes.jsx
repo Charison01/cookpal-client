dc6120d26919170b25f6724714788fcdf8717fe5
kelvin kimaru <kelvinkimaru@gmail.com>
2023-10-31 21:08:43 +0300

    updated recipes page

--- src/Pages/MyRecipes.jsx
+++ src/Pages/MyRecipes.jsx
@@ -22,5 +22,12 @@ export default function MyRecipes() {
     }
   }, []);
 
-  return <section>MyRecipes</section>;
+  return (
+    <section className="px-2 flex-1 lg:max-w-[75%] lg:mx-auto bg-[url('https://images.unsplash.com/photo-1490818387583-1baba5e638af?auto=format&fit=crop&q=80&w=1932&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-center bg-opacity-[65%]">
+      <h1 className="font-bold text-2xl lg:text-3xl text-gray-600 py-2 px-2">
+        {" "}
+        My Recipes
+      </h1>
+    </section>
+  );
 }
