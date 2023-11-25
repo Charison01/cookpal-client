34f1665fed8c85950302c0531677a7922382bbf5
Donvine Mugendi <donvinemugendi@gmail.com>
2023-11-26 02:53:10 +0300

    remove skeleton loader in help page

--- src/Pages/Help.jsx
+++ src/Pages/Help.jsx
@@ -1,19 +1,15 @@
-import React from "react";
-import SkeletonRecipeDetails from "../Components/SkeletonRecipeDetails";
 export default function Help() {
   return (
-    // <div className="px-2 flex-1 lg:max-w-[75%] lg:mx-auto bg-[url('https://images.unsplash.com/photo-1490818387583-1baba5e638af?auto=format&fit=crop&q=80&w=1932&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-center ">
-    //   <div className="py-2 px-2  text-gray-600 border bg-white">
-    //     <h2 className="text-2xl lg:text-3xl font-bold">
-    //       You&apos;ve found our secret page
-    //     </h2>
-    //     <p>
-    //       Right now, there is nothing much to see but soon it will be a home to
-    //       a beautiful page!
-    //     </p>
-    //   </div>
-
-    // </div>
-    <SkeletonRecipeDetails />
+    <div className="px-2 flex-1 lg:max-w-[75%] lg:mx-auto bg-[url('https://images.unsplash.com/photo-1490818387583-1baba5e638af?auto=format&fit=crop&q=80&w=1932&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D')] bg-cover bg-center ">
+      <div className="py-2 px-2  text-gray-600 border bg-white">
+        <h2 className="text-2xl lg:text-3xl font-bold">
+          You&apos;ve found our secret page
+        </h2>
+        <p>
+          Right now, there is nothing much to see but soon it will be a home to
+          a beautiful page!
+        </p>
+      </div>
+    </div>
   );
 }
